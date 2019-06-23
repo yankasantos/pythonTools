d52255ec9bbeb27895732ff5d63d9d82b9054cdd
yankasantos <yankasantos0107@gmail.com>
2019-06-22 22:37:21 -0300

    permitindo a inserção de dados pelo usuário e a renderização das informações coletadas

---
+++ contact.py
@@ -0,0 +1,7 @@
+name = input('Digite seu nome:')
+lastName = input('Digite seu sobrenome:')
+age = input('Digite sua idade:')
+print(name + lastName + age)#concatena os valores sem quebra de linha e sem espaçamento
+print(name)#renderiza o nome individualmente
+print(lastName)#renderiza o sobrenome individualmente
+print(age)#renderiza a idade individualmente
